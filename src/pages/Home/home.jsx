e4ff90be07484cbb68b8eca6123d3ef95e46fcff
MUHAMMAD AIMAN <m.aiman189@gmail.com>
2023-10-18 17:28:49 +0800

    Create new page

---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+      <>
+      <div className="text-4xl text-center py-8 px-2">
+        <h3>INTRODUCTION</h3>
+      </div>
+      <div className="text-lg text-center py-8 px-2">
+        <p>Hello and welcome to my page</p>
+        <p>My name is <>MUHAMMAD AIMAN</></p>
+      </div>
+      </>
+    )
+  }
