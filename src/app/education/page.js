56c547752d540df477257caa6c6a9bb43146872e
MUHAMMAD AIMAN <m.aiman189@gmail.com>
2023-10-25 16:59:43 +0800

    Add content in education

--- src/app/education/page.js
+++ src/app/education/page.js
@@ -1,8 +1,15 @@
 export default function Education() {
     return (
       <>
-      <div>
-        <p>Education</p>
+      <div >
+        <div className={`bg-[url('../../public/bg/home/home_bg.jpg')] bg-cover bg-center h-full py-40`}>
+          <h3 className="text-6xl text-left font-bold py-8 pl-20 pr-80 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Education</h3>
+        </div>
+      </div>
+      <div className="text-lg text-left py-20 px-20">
+        <h3 className="font-bold text-xl">DIPLOMA IN COMPUTER SCIENCE (UiTM Kampus Sungai Petani, Cawangan Kedah)</h3>
+        <p className="text-lg">CGPA: 3.89</p>
+        <br />
       </div>
       </>
     )
